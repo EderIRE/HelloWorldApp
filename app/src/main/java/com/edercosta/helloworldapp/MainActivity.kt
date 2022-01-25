9e9a8d333c499523a8aba25025445b622ae6ae3f
EderIRE <71097226+EderIRE@users.noreply.github.com>
2022-01-25 17:18:42 +0000

    Initial commit

---
+++ app/src/main/java/com/edercosta/helloworldapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.edercosta.helloworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
